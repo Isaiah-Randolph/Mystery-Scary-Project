f9903825c62512213be21985f1fe8d3ccc91544f
windastella <nik96mirza@gmail.com>
2016-04-22 13:21:19 +0800

    Resized the minimap so that it show the entire level. Still need a better entity postion script.

---
+++ Assets/Script/Misc/FollowTargetHeight.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTargetHeight : MonoBehaviour {
+	public Transform target;
+	public Vector3 offset = new Vector3(0f, 7.5f, 0f);
+
+
+	private void LateUpdate()
+	{
+		Vector3 pos = new  Vector3(transform.position.x, target.position.y, transform.position.z);
+		transform.position = pos + offset;
+	}
+}
